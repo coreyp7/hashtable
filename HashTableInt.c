601bf552f0bd116350087dca80549e260086f744
Corey Pierce <coreypierce11@gmail.com>
2025-01-02 12:51:41 -0500

    Fixed it not handling collisions correctly, some tests

--- HashTableInt.c
+++ HashTableInt.c
@@ -26,10 +26,13 @@ HashTableInt create_HashTableInt(int size){
 
 // https://stackoverflow.com/questions/664014/what-integer-hash-function-are-good-that-accepts-an-integer-hash-key
 int _hash(int x){
+	/*
 	x = ((x >> 16) ^ x) * 0x45d9f3b;
 	x = ((x >> 16) ^ x) * 0x45d9f3b;
  	x = (x >> 16) ^ x;
 	return x;	
+	*/
+	return 0;
 }
 
 int put(HashTableInt* table, int key, int value){
@@ -43,16 +46,41 @@ int put(HashTableInt* table, int key, int value){
 	}
 
 	HashTableIntNode newNode = {key, value, 1};
-	HashTableIntNode* node = &table->array[index];
+	HashTableIntNode* node; //= &table->array[index];
 
+
+	printf("index is %i\n", index);
+	for(int i=index; i<table->size; i++){
+		node = &table->array[i];
+
+		if(node->key == key){
+			// Update the key with its new value
+			table->array[i] = newNode;
+			node = NULL;
+			return 0;
+		}
+
+		if(!node->alive){
+			// This is an empty node, we can put K/V here.
+			table->array[i] = newNode;
+			return 0;
+		}	
+		
+	}
+
+	/*
 	while(node < table->array + (sizeof(HashTableIntNode)*table->size)){
 		if(!node->alive || node->key == key){
+			// TODO: change this to a for loop so we can easily
+			// print/observe the current index.
+			printf("%i || %i, setting at node\n", !node->alive, node->key == key);
 			table->array[index] = newNode;
 			node = NULL;
 			return 0;
 		}
 		node++;
 	}
+	*/
 	return 1;
 }
 
@@ -76,11 +104,38 @@ int* get(HashTableInt* table, int key){
 	return NULL;
 }
 
+void print_dict_array(HashTableInt* table){
+	// NOTE: only printing alive nodes
+	for(int i=0; i<table->size; i++){
+		HashTableIntNode node = table->array[i];
+		//if(node.alive){
+			printf("index %i: (%i, %i, %i)\n", i, node.key, node.value, node.alive);
+		//}
+	}
+}
+
 int main(){
+	// Testing collisions (hash function should be changed to always return 0)
 	HashTableInt table = create_HashTableInt(100);
-	put(&table, 4, 248724);
+	printf("put return: %i\n", put(&table, 4, 248724)); // index 0
+	print_dict_array(&table);
 	assert(get(&table, 4) != NULL);
 	assert(*get(&table, 4) == 248724);
+
+	put(&table, 2, 98654); // index 1
+	print_dict_array(&table);
+	assert(get(&table, 2) != NULL);
+	assert(*get(&table, 2) == 98654); // will linear search until found
+	put(&table, 2, 7); // index 1, replace the old value
+	assert(*get(&table, 2) == 7); // will linear search until found
+	
+	/*
+	Remaining scenarios:
+	- when the array is too full (use load factor calc I believe)
+	- when there's collisions
+		- change the hash function to just return 0 everytime and
+		do it alot
+	*/
 }
 
 
